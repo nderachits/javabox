91c574d552f29fd034ff26f2822f197ebee0104a
Mikalai Dzerachyts <n.derachits@gmail.com>
2013-11-14 21:08:49 +0200

    Simple test was added

---
+++ springapp1/src/main/java/com/mikalai/Simple.java
@@ -0,0 +1,11 @@
+package com.mikalai;
+
+/**
+ * User: nike
+ * Date: 11/14/13
+ */
+public class Simple {
+    public boolean doNothing() {
+        return true;
+    }
+}
---
+++ springapp1/src/test/java/com/mikalai/SimpleTest.java
@@ -0,0 +1,22 @@
+package com.mikalai;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * User: nike
+ * Date: 11/14/13
+ */
+
+public class SimpleTest {
+
+    private Simple app = new Simple();
+    @Test
+    public void testNothing() throws Exception {
+
+        assertTrue(app.doNothing());
+
+    }
+}
