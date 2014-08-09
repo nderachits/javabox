0841137f7ea12e16fd00e74465c5c5fd58e471e0
nderachits <n.derachits@gmail.com>
2014-08-10 00:01:20 +0300

    deploy to cloudbees by jenkins

--- springapp1/src/main/java/hello/Application.java
+++ springapp1/src/main/java/hello/Application.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,6 +25,15 @@ public class Application {
         for (String beanName : beanNames) {
             System.out.println(beanName);
         }
+    }
+
+    @Bean
+    public ServerProperties myServerProperties() {
+        ServerProperties p = new ServerProperties();
+        String portStr = System.getProperty("app.port");  // CloudBees Environment Variable for local port
+        int port = (portStr != null) ? Integer.parseInt(portStr) : 8080;
+        p.setPort(port);
+        return p;
     }
 
 }
